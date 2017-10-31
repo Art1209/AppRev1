640fa70494c8c8c3548e5e8260f98c2bb74ffde4
art102 <a5903281@ya.ru>
2017-10-31 17:58:38 +0300

    temp

--- src/main/java/AppRev1/highLevelApp/config/secutity/CustomBasicAuthenticationFilter.java
+++ src/main/java/AppRev1/highLevelApp/config/secutity/CustomBasicAuthenticationFilter.java
@@ -5,19 +5,24 @@ import AppRev1.highLevelApp.persistence.entity.Token;
 import AppRev1.highLevelApp.persistence.service.PersonService;
 import AppRev1.highLevelApp.persistence.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by aalbutov on 27.10.2017.
  */
 @Component
-public class CustomBasicAuthenticationFilter extends BasicAuthenticationFilter {
+public class CustomBasicAuthenticationFilter extends BasicAuthenticationFilter implements AuthenticationSuccessHandler {
 
 
     @Autowired
@@ -26,8 +31,7 @@ public class CustomBasicAuthenticationFilter extends BasicAuthenticationFilter {
     @Autowired
     PersonService personService;
 
-    @Autowired
-    TokenService tokenService;
+
 
     @Autowired
     public CustomBasicAuthenticationFilter(final AuthenticationManager authenticationManager) {
@@ -38,13 +42,24 @@ public class CustomBasicAuthenticationFilter extends BasicAuthenticationFilter {
     protected void onSuccessfulAuthentication(HttpServletRequest request,
                                               HttpServletResponse response, Authentication authResult) {
         Person person = personService.getPerson(((CustomPrincipal)authResult.getPrincipal()).getId());
+        Token token = null;
+        List<Token> tokens;
+        if ((tokens = person.getTokenList())!=null){
+            for (Token token1 : tokens){
+                if ((token1.getExpires().getTime()-System.currentTimeMillis())>0){
+                    token = token1;
+                    break;
+                }
 
-        Token token = tokenProvider.generateToken(person);
-        tokenService.addToken(token);
+            }
+        }
+        if (null==token)token = tokenProvider.generateToken(person.getId());
         //send token in the response
-        response.setHeader("X-AUTH-TOKEN" , token.getToken());
-
-
+        response.setHeader(HttpHeaders.AUTHORIZATION , token.getToken());
     }
 
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        onSuccessfulAuthentication(request, response, authentication);
+    }
 }
